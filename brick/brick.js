f049d6a6d1542c3df756fcc477163e5d8a98ce16
junyiz <junyime@qq.com>
2024-04-21 22:13:53 +0800

    bricks game

--- brick/brick.js
+++ brick/brick.js
@@ -1,6 +1,5 @@
 (function(doc){
-    var box = doc.querySelector('#bricks'),
-        ball = doc.querySelector('#ball'),
+    var ball = doc.querySelector('#ball'),
         board = doc.querySelector('#board'),
         bricks = doc.querySelectorAll('#bricks li');
 
@@ -12,45 +11,37 @@
         ball.style.left = left + MX + 'px';
         ball.style.top = top + MY + 'px';
     }
-    
-    var target = bricks;
-    // 将target转换成真正的数组
-    // target = Array.prototype.slice.call(target, 0);
 
     function impact() {
          for (var i = 0; i < target.length; i++) {
 
-             var curStyle =getComputedStyle(target[i]), 
-                 t_left = parseInt(target[i].style.left),
-                 t_top = parseInt(target[i].style.top),
+             var curStyle = getComputedStyle(bricks[i]), 
+                 t_left = parseInt(bricks[i].style.left),
+                 t_top = parseInt(bricks[i].style.top),
                  t_width = parseInt(curStyle['width']) + 2,
                  t_height = parseInt(curStyle['height']) + 2,
                  b_left = parseInt(ball.style.left),
                  b_top = parseInt(ball.style.top),
-                 b_width = 12,
-                 b_height = 12;
+                 b_width = 12;
 
              // 砖块是否已被打掉
-             var isDis =target[i].style.display != 'none';  
+             var isDis = bricks[i].style.display != 'none';  
 
              // 与砖块底边相撞
              if (b_top <= t_top + t_height && b_left >= t_left - 5 && b_left < t_left + t_width - 1 && isDis) {
-                 target[i].style.display = 'none';
-                 console.log('bottom');
+                 bricks[i].style.display = 'none';
                  MY = -MY;
                  break;
                  
              // 与砖块左边相撞
              } else if (b_top >= t_top && b_top <= t_top + t_height && b_left >= t_left - b_width && b_left <= t_left && isDis) {
-                 target[i].style.display = 'none';
-                 console.log('left');
+                 bricks[i].style.display = 'none';
                  MX = -MX;
                  break;
                  
              // 与砖块右边相撞
              } else if (b_top > t_top && b_top < t_top + t_height && b_left > t_left && b_left < t_left + t_width && isDis) {
-                 target[i].style.display = 'none';
-                 console.log('right');
+                 bricks[i].style.display = 'none';
                  MX = -MX;
                  break;
 
@@ -90,7 +81,7 @@
     
     doc.querySelector('#board').onclick = start;
 
-    //弹板的移动 
+    // 弹板的移动 
     window.onkeydown = function(e) {
         var boardLeft = parseInt(board.style.left);
         if (e.keyCode == 37 && boardLeft > 0) {
